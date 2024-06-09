ec709e8c29f90ed50e3c5e165e853c27df91f308
Jack <9019233@student.zadkine.nl>
2024-06-09 13:49:58 +0200

    Started creating HTML documents

---
+++ Website/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Portfolio Jack van Bommel</title>
+</head>
+<body>
+	<?php include "./general_site_files/navbar.html" ?>
+
+	<main>
+		<h1>Portfolio Jack van Bommel</h1>
+		
+		<p>Mijn werkervaring bekijken? Klik dan</p><a href="#">hier!</a>
+		<p>Mijn laatste project: </p>
+
+		<div class="homepage_text">
+			<p>Mijn naam is Jack van Bommel en ik ben een 19 jarige MBO 4 Software Development Student.</p>
+			<p>Sinds jongs af aan heb ik al een passie gehad voor technologie en computers die ik meeneem in al het werk dat ik maak.</p>
+		</div>
+
+		<div class="homepage_links">
+			<ul>
+				<li><a href="#">LinkedIn</a></li>
+				<li><a href="#">Github</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</div>
+	</main>
+
+	<?php include "./general_site_files/footer.html" ?>
+</body>
+</html>
---
+++ Website/knowledge_skills.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Kennis & Vaardigheden</title>
+</head>
+<body>
+	<?php include "./general_site_files/navbar.html" ?>
+
+	<h1>Kennis & Vaardigheden</h1>
+
+	<main>
+		<div class="skills_backend">
+			<h2>Backend kennis</h2>
+			<ul>
+				<li><a href="#">PHP</a>, zowel procedural als Object Georiënteerd Programmeren</li>
+				<li>Ervaring met <a href="#">MySQL</a> database beheer in <a href="#">PHPMyAdmin</a></li>
+				<li>Basis kennis van <a href="#">C#</a></li>
+				<li>Ervaring met API integratie</li>
+			</ul>
+		</div>
+
+		<div class="skills_frontend">
+			<ul>
+				<li>Ervaring met het gebruik van <a href="#">HTML 5</a> om webpagina's te maken</li>
+				<li>Basis kennis van HTML 5 gebruik in <a href="#">TWIG Template Management</a></li>
+				<li>Ervaring met <a href="#">CSS</a> en <a href="#">SASS</a></li>
+				<li><a href="#">JavaScript</a> beginner kennis</li>
+			</ul>
+		</div>
+
+		<div class="skills_version">
+			<ul>
+				<li>Ervaring in het gebruik van <a href="#">Github</a> voor versie controle en samenwerking</li>
+				<li>Project beheer bijhehouden met de <a href="#">SCRUM</a> methode</li>
+				<li>Ervaring met het bijhouden van diverse project documentatie zoals design documenten, project plannen en test cases</li>
+			</ul>
+		</div>
+
+		<div class="skills_programs">
+			<ul>
+				<li><a href="#">Visual Studio Code</a> met diverse plugins</li>
+				<li><a href="#">Visual Studio</a> voor C# projecten</li>
+				<li>Basis kennis van <a href="#">Unity</a> voor game development, in samenwerking met Visual Studio</li>
+				<li><a href="#">StarUML</a> voor Class Diagrammen en Use Case Diagrammen</li>
+			</ul>
+		</div>
+	</main>
+
+	<?php include "./general_site_files/footer.html" ?>
+</body>
+</html>
